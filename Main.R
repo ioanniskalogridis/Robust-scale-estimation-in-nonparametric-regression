6057e2fea4b9c92c8d78fa9ade9cb92ad18ed662
Ioannis Kalogridis <ioannis.kalogridis@kuleuven.be>
2024-11-20 09:57:18 +0100

    Update Main.R

--- Main.R
+++ Main.R
@@ -6,14 +6,14 @@ require(SparseM)
 rob.scale <- function(x, y, tuning.c = 1e-04, max.iter = 500, pen.reg = 2, pen.scale = 2, lambda = NULL,
                       toler = 1e-08){
   
-  quan.smsp <- function(x, y, tuning = 1e-04, maxit = 500, m = 2, 
+  l1.smsp <- function(x, y, tuning = 1e-04, maxit = 500, m = 2, 
                         lambda = NULL){
     
     rho.abs <-  function(x, tuning = 1e-04) ifelse(abs(x) <= tuning, 0.5*x^2/tuning, abs(x))
     psi.abs <- function(x, tuning = 1e-04) ifelse(abs(x) <= tuning, x/tuning, sign(x))
     weight.abs <- function(x, tuning = 1e-04) ifelse(abs(x) <= tuning, 1/tuning, sign(x)/x)
     
-    quan.step <- function(X, y, tuning = tuning.c , tol = toler, lambda, Pen.matrix, resids.in, maxit = max.iter){
+    l1.step <- function(X, y, tuning = tuning.c , tol = toler, lambda, Pen.matrix, resids.in, maxit = max.iter){
       
       ic = 0
       istop = 0
@@ -35,10 +35,46 @@ rob.scale <- function(x, y, tuning.c = 1e-04, max.iter = 500, pen.reg = 2, pen.s
       resids = y - fitted.values
       hat.values = diag(X%*%SparseM::solve(M1, X.s))
       # hat.values = sum( t(SparseM::solve(M1, X.s)) * X )/length(y)
+      return(list(resids = resids, beta.hat = v1, hat.values = hat.values, 
+                  weights = weights1, fitted = fitted.values, ic = ic, check = check ) )
     }
-    return(list(resids = resids, beta.hat = v1, hat.values = hat.values, 
-                weights = weights1, fitted = fitted.values, ic = ic, check = check ) )
+    n <- length(y)
+    
+    x.t <- (x-min(x))/(max(x)-min(x))
+    
+    b.basis <- create.bspline.basis(rangeval = c(0, 1), breaks = unique(x.t), norder = 2*pen.reg)
+    b.basis.e <- eval.basis(b.basis, x.t)
+    
+    if(pen.reg==1){
+      P <- diff(diag(n), differences = 1)
+      Pen.matrix <- t(P)%*%diag(1/(diff(x, differences = 1)))%*%P
+    } else {
+      Pen.matrix <- bsplinepen(b.basis, Lfdobj = pen.reg) }
+    
+    fit.in <- smooth.spline(x, y)
+    resids.in <- y - predict(fit.in, x.t)$y
+    GCV <- function(lambda){
+      fit.r <- quan.step(X = b.basis.e,  y = y, lambda = lambda, Pen.matrix = Pen.matrix,
+                         alpha = alpha, tuning = tuning, resids.in = resids.in, maxit = maxit)
+      GCV.scores <-  mean( fit.r$weights*((fit.r$resids)^2) /((1-fit.r$hat.values)^2) )
+      return(GCV.scores)
+    }
+    cand.values <- exp(seq(log(1e-12), log(3), len = 50))
+    GCV.values <- sapply(cand.values, FUN = GCV)
+    lambda1 = cand.values[which.min(GCV.values)]
+    
+    lambda1 <- ifelse(is.null(lambda), lambda1, lambda)
+    fit.rf <- quan.step(X = b.basis.e,  y = y, lambda = lambda1, Pen.matrix = Pen.matrix, 
+                        tuning = tuning.c, resids.in = resids.in, maxit = max.iter)
+    
+    mu <- as.vector(b.basis.e%*%fit.rf$beta.hat)
+    
+    return(list(x = x, mu = mu, weights = fit.rf$weights, Pen.matrix = Pen.matrix, fitted = fit.rf$fitted, 
+                lambda = lambda1, ic = fit.rf$ic, resids = fit.rf$resids, tol.f = fit.rf$check))
   }
+  
+  
+  
 }
 
 
