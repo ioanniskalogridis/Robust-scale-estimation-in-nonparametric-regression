7943218ca6a694124c2fec5923c24f4036d926a3
Ioannis Kalogridis <ioannis.kalogridis@kuleuven.be>
2024-12-09 08:55:16 +0100

    Update Monte-Carlo.R

--- Monte-Carlo.R
+++ Monte-Carlo.R
@@ -2,10 +2,11 @@ require(fda)
 require(sn)
 require(EnvStats)
 
-nrep <- 500
+nrep <- 50
 n <- 100
 m.scale.ls <- matrix(NA, nrow = n, ncol = nrep)
 m.scale.rob1 = m.scale.rob2 = m.scale.rob5 = m.scale.rob10 <- matrix(NA, nrow = n, ncol = nrep)
+m.reg <- matrix(NA, nrow = n, ncol = nrep)
 
 mse.scale.ls <- rep(NA, nrep)
 mse.scale.rob1 = mse.scale.rob2 = mse.scale.rob5 = mse.scale.rob10 <- rep(NA, nrep)
@@ -30,17 +31,18 @@ for(k in 1:nrep){
   fit.rob10 <- rob.scale(x, y, v = 10)
   fit.ls <- rob.scale(x, y, v = 50)
   
-  mse.scale.ls[k] <- mean((fit.ls$mu-f1(x))^2)
-  mse.scale.rob1[k] <- mean((fit.rob1$mu-f1(x))^2)
-  mse.scale.rob2[k] <- mean((fit.rob2$mu-f1(x))^2)
-  mse.scale.rob5[k] <- mean((fit.rob5$mu-f1(x))^2)
-  mse.scale.rob10[k] <- mean((fit.rob10$mu-f1(x))^2)
+  mse.scale.ls[k] <- mean((fit.ls$scale.est-f1(x))^2)
+  mse.scale.rob1[k] <- mean((fit.rob1$scale.est-f1(x))^2)
+  mse.scale.rob2[k] <- mean((fit.rob2$scale.est-f1(x))^2)
+  mse.scale.rob5[k] <- mean((fit.rob5$scale.est-f1(x))^2)
+  mse.scale.rob10[k] <- mean((fit.rob10$scale.est-f1(x))^2)
   
-  m.scale.ls[, k] <- fit.ls$mu
-  m.scale.rob1[, k] <- fit.rob1$mu
-  m.scale.rob2[, k] <- fit.rob2$mu
-  m.scale.rob5[, k] <- fit.rob5$mu
-  m.scale.rob10[, k] <- fit.rob10$mu
+  m.scale.ls[, k] <- fit.ls$scale.est
+  m.scale.rob1[, k] <- fit.rob1$scale.est
+  m.scale.rob2[, k] <- fit.rob2$scale.est
+  m.scale.rob5[, k] <- fit.rob5$scale.est
+  m.scale.rob10[, k] <- fit.rob10$scale.est
+  m.reg[, k] <- fit.ls$reg.est
 }
 
 mean(mse.scale.rob1, na.rm = TRUE)*100;sd(mse.scale.rob1, na.rm = TRUE)/sqrt(nrep)*100
@@ -49,10 +51,15 @@ mean(mse.scale.rob5, na.rm = TRUE)*100; sd(mse.scale.rob5, na.rm = TRUE)/sqrt(nr
 mean(mse.scale.rob10, na.rm = TRUE)*100; sd(mse.scale.rob10, na.rm = TRUE)/sqrt(nrep)*100
 mean(mse.scale.ls, na.rm = TRUE)*100;  sd(mse.scale.ls, na.rm = TRUE)/sqrt(nrep)*100
 
-matplot(x, m.scale.ls, type = "l", lwd = 3, lty = 1, cex.axis = 2.5, cex.lab = 2.5, cex = 3, ylab = "y",
-        col = "gray", ylim = c(0.3, 2.5)) ; grid()
+par(mar = c(6,6.5,2,2), mgp = c(4.5, 1.5, 0))
+matplot(x, m.scale.ls[, 1:20], type = "l", lwd = 3, lty = 1, cex.axis = 2.5, cex.lab = 2.5, cex = 3,
+        col = "gray", ylim = c(0.3, 1.7), ylab = "") ; grid()
 curve(f1(x), add= TRUE, lwd = 3, col = "black")
-matplot(x, m.scale.rob2, type = "l", lwd = 3, lty = 1, cex.axis = 2.5, cex.lab = 2.5, cex = 3, ylab ="y",
-        col = "gray", ylim = c(0.3, 2.5)); grid()
+matplot(x, m.scale.rob2[ ,1:20], type = "l", lwd = 3, lty = 1, cex.axis = 2.5, cex.lab = 2.5, cex = 3,
+        col = "gray", ylim = c(0.3, 1.7), ylab = ""); grid()
 curve(f1(x), add= TRUE, lwd = 3, col = "black")
+
+matplot(x, m.reg, type = "l", lwd = 3, lty = 1, cex.axis = 2.5, cex.lab = 2.5, cex = 3,
+        col = "gray", ylab = ""); grid()
+curve(cos(2*pi*x), add= TRUE, lwd = 3, col = "black")
 
