5471a92a18d9f953a6e13ae5ad71ba62f749cec3
Ioannis Kalogridis <ioannis.kalogridis@kuleuven.be>
2025-01-24 09:39:43 +0100

    Update Application.R

--- Application.R
+++ Application.R
@@ -4,18 +4,19 @@ require(worldmet)
 require(lubridate)
 require(openair)
 
-site.codes <- importMeta(source = "aurn", all = FALSE, year = NA, duplicate = FALSE)
-kc1 <- importAURN(site = "SCN2", year = 2024) #nott OK, 
-data <- select(kc1, nox, air_temp)
-data <- data[complete.cases(data),]
-data <- data[order(data$air_temp),]
-dim(data)
+# site.codes <- importMeta(source = "aurn", all = FALSE, year = NA, duplicate = FALSE)
+# kc1 <- importAURN(site = "SCN2", year = 2024)  
+# data <- select(kc1, nox, air_temp)
+# data <- data[complete.cases(data),]
+# data <- data[order(data$air_temp),]
+# dim(data)
+data <- read.table(file = file.choose(), header = T)
 y <- data$nox[1:7954]
 x <- data$air_temp[1:7954]
 
 par(mar = c(6,6.5,2,2), mgp = c(4.5, 1.5, 0))
 plot(x,y, cex.axis = 2.5, cex.lab = 2.5, cex = 2.5, pch = 19, col = "gray",
-     ylab = "NOx (PPB)", xlab = "Temperature (\u00B0C)") ; grid()
+     ylab = "NOx (PPB)", xlab = "Temperature (\u00B0C)", ylim = c(0, 280)) ; grid()
 fit.ls1 <- rob.scale(x, y, v = 200, type = "ls")
 fit.rob1 <- rob.scale(x, y, v = 1, type = "rob")
 
@@ -24,13 +25,13 @@ plot(x, fit.ls1$scale.est, lwd = 3, col = "black", type = "l", ylim = c(0, 50),
      cex.axis = 2.5, cex.lab = 2.5, cex = 2.5, ylab = "Estimates", xlab = "Temperature (\u00B0C)")
 lines(x, fit.rob1$scale.est, lwd = 3, col = "blue"); grid()
 
-cut.off <- 2.5
+cut.off <- 2.6
 sum(abs(fit.ls1$st.resids)>cut.off)
 sum(abs(fit.rob1$st.resids)>cut.off)
-plot(x,y, cex.axis = 2.5, cex.lab = 2.5, cex = 2.5, pch = 19, col = "gray",
-     ylab = "NOx (PPB)", xlab = "Temperature (\u00B0C)") ; grid()
+plot(x[abs(fit.ls1$st.resids)<=cut.off],y[abs(fit.ls1$st.resids)<=cut.off], cex.axis = 2.5, cex.lab = 2.5, cex = 2.5, pch = 19, col = "gray",
+     ylab = "NOx (PPB)", xlab = "Temperature (\u00B0C)", ylim = c(0, 280)) ; grid()
 points(x[abs(fit.rob1$st.resids)>cut.off], y[abs(fit.rob1$st.resids)>cut.off], 
-       col = "red", cex = 2.5, pch = 19)
+       col = "red", cex = 1.5, pch = 19)
 
 x.r <- x[which(fit.rob1$st.resids<=cut.off)]
 y.r <- y[which(fit.rob1$st.resids<=cut.off)]
